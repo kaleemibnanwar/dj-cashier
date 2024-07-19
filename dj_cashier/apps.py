64fdf081389f50e07717915fc684342f338a1f4b
Kaleem Ibn Anwar <kaleemibnanwar@gmail.com>
2024-07-19 05:02:07 +0500

    1.1

--- dj_cashier/apps.py
+++ dj_cashier/apps.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class DjCashierConfig(AppConfig):
+    default_auto_field = getattr(settings, 'DEFAULT_AUTO_FIELD', 'django.db.models.BigAutoField')
     name = 'dj_cashier'
--- dj_cashier/views.py
+++ dj_cashier/views.py
@@ -7,7 +7,7 @@ from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 import stripe
-from .models import StripeSubscription, StripeSubscriptionItem
+from .models import Subscription, SubscriptionItem
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -40,8 +40,8 @@ class StripeWebhookView(View):
 
         customer = self.User.objects.filter(stripe_id=subscription_response["customer"]).first()
 
-        if customer and not StripeSubscriptionItem.objects.filter(stripe_id=subscription_response.id).exists():
-            subscription_object = StripeSubscription.objects.create(
+        if customer and not SubscriptionItem.objects.filter(stripe_id=subscription_response.id).exists():
+            subscription_object = Subscription.objects.create(
                 user=customer,
                 stripe_id=subscription_response.id,
                 status=subscription_response.status,
@@ -56,7 +56,7 @@ class StripeWebhookView(View):
                 interval = item["plan"]["interval"]
                 quantity = item["quantity"]
 
-                StripeSubscriptionItem.objects.create(
+                SubscriptionItem.objects.create(
                     user=customer,
                     subscription=subscription_object,
                     price_id=price_id,
@@ -68,16 +68,16 @@ class StripeWebhookView(View):
         subscription_data = event["data"]["object"]
         try:
             print("SUBSCRIPTION_DELETED: ", subscription_data["id"])
-            subscription = StripeSubscription.objects.get(stripe_id=subscription_data["id"])
+            subscription = Subscription.objects.get(stripe_id=subscription_data["id"])
             subscription.set_deleted_status()
-        except StripeSubscription.DoesNotExist:
+        except Subscription.DoesNotExist:
             print(f"Subscription with stripe_id {subscription_data['id']} not found.")
 
     def handle_customer_subscription_updated(self, event):
         subscription_data = event["data"]["object"]
         try:
             print("SUBSCRIPTION_UPDATED: ", subscription_data["id"])
-            subscription = StripeSubscription.objects.get(stripe_id=subscription_data["id"])
+            subscription = Subscription.objects.get(stripe_id=subscription_data["id"])
 
             subscription.trial_ends_at = timestamps_to_str(subscription_data["trial_end"])
             subscription.quantity = subscription_data["quantity"]
@@ -86,18 +86,18 @@ class StripeWebhookView(View):
             subscription.status = subscription_data["status"]
             subscription.save()
             print(f"Subscription {subscription.stripe_id} updated.")
-        except StripeSubscription.DoesNotExist:
+        except Subscription.DoesNotExist:
             print(f"Subscription with stripe_id {subscription_data['id']} not found.")
 
     def handle_invoice_payment_succeeded(self, event):
         invoice_data = event["data"]["object"]
         try:
-            subscription = StripeSubscription.objects.get(stripe_id=invoice_data["subscription"])
+            subscription = Subscription.objects.get(stripe_id=invoice_data["subscription"])
             """
             GET Subscription items and then plan of that item. as done in User model of Dashboard
             This way you can create a detailed notification or email that recurring payment of user subscription is captured.
             """
             print(f"Payment for subscription {subscription.stripe_id} succeeded.")
-        except StripeSubscription.DoesNotExist:
+        except Subscription.DoesNotExist:
             print(f"Subscription associated with invoice {invoice_data['subscription']} not found.")
 
